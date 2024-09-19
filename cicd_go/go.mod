88991b6f386b422f4b3924451674aabcc2e7e4f8
Olubusolami Sogunle <busolasogunle@gmail.com>
2024-09-19 14:29:39 +0100

    Initial go

---
+++ cicd_go/go.mod
@@ -0,0 +1,3 @@
+module github.com/Olubusolami-R/cicd_for_packages/cicd_go
+
+go 1.22.1
---
+++ cicd_go/testLib.go
@@ -0,0 +1,8 @@
+package cicd_go
+
+import "fmt"
+
+func testLib(){
+	fmt.Println("Testing out the library in go wooooohoooo")
+}
+
